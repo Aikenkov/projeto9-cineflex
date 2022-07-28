9a7e94065f259828dcf5c9a379240049ed7e6a66
Fabio <Fabio.rod.silva.sqpr@gmail.com>
2022-07-28 13:56:58 -0300

    tela inicial layout pronto

--- src/components/MoviesOptions.js
+++ src/components/MoviesOptions.js
@@ -1,15 +1,32 @@
 import styled from "styled-components"
 import axios from 'axios';
+import { useState, useEffect } from 'react';
 
+/* "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM1TYXF-IlqTCvBs408b2ZW2wL8wK59S4yMw&usqp=CAU.png"
+ */
 function MovieTemplate() {
+    const [movies, setMovies] = useState([])
 
-
+    useEffect(() => {
+        const promise = axios.get('https://mock-api.driven.com.br/api/v5/cineflex/movies')
+        promise.then(res => {
+            setMovies(res.data)
+        })
+    }, [])
 
     return (
-        <Movie>
+        <Container>
             <h1>Selecione o filme</h1>
-            <div>Selecione o filme</div>
-        </Movie>
+            <MoviesList>
+                {movies.map(item => {
+                    return (
+                        <Movie key={item.id}>
+                            <img src={item.posterURL} alt='tome' />
+                        </Movie>
+                    )
+                })}
+            </MoviesList>
+        </Container>
     )
 }
 
@@ -18,9 +35,42 @@ export default function MoviesOptions() {
         <MovieTemplate />
     )
 }
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 67px;
+
+    h1{
+        font-weight: 400;
+        font-size: 24px;
+        text-align: Center;
+        padding: 55px 0 ;
+    }
+
+`
+
+const MoviesList = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    margin: 0 20px;
+
+`
 
 const Movie = styled.div`
-background-color: black;
-height: 100px;
-width: 100px;
+    box-sizing: border-box;
+    margin: 5px 15px;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.1);
+    border-radius: 3px;
+    height: 209px;
+    width: 145px;
+    img{
+        height: 193px;
+        width: 129px;
+    }
 `
