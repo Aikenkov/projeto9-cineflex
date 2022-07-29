943ca271f30490fe60f12b8ecc9a3fa3c513a4fb
Fabio <Fabio.rod.silva.sqpr@gmail.com>
2022-07-29 17:13:50 -0300

    Terminei o layout. Começar a fazer o clique dos botões

--- src/components/MoviesSessions.js
+++ src/components/MoviesSessions.js
@@ -8,15 +8,15 @@ import Movie from "../theme/Movie";
 
 
 function MovieSessionsTemplate() {
-    const [sessions, setSessions] = useState([])
-
+    const [sessions, setSessions] = useState([]);
+    const [days, setDays] = useState(false);
     const params = useParams();
-
 
     useEffect(() => {
         const promise = axios.get(`https://mock-api.driven.com.br/api/v7/cineflex/movies/${params.idFilme}/showtimes`)
         promise.then(res => {
             setSessions(res.data)
+            setDays(res.data.days)
         })
     }, [])
 
@@ -24,6 +24,25 @@ function MovieSessionsTemplate() {
     return (
         <Container>
             <h1>Selecione o horário</h1>
+            {days != false ? (
+                <AllSessions>
+                    {days.map(item => {
+                        return (
+                            <WeekDaySessions key={item.id}>
+
+                                <p>{item.weekday} - {item.date}</p>
+                                <div>
+                                    {(item.showtimes).map(value => {
+                                        return <button key={value.id}>{value.name} </button>
+                                    })}
+                                </div>
+
+                            </WeekDaySessions>
+                        )
+                    })}
+
+                </AllSessions>)
+                : (<h1>Carregando...</h1>)}
             <Footer>
                 <TesteMovie>
                     <img src={sessions.posterURL} />
@@ -37,6 +56,49 @@ function MovieSessionsTemplate() {
 export default function MovieSessions() {
     return <MovieSessionsTemplate />
 }
+
+
+const WeekDaySessions = styled.div`
+       
+        height: 100px;
+        margin-left: 20px;
+        margin-bottom: 23px;
+        
+        & > div {
+            display: flex;
+        }
+    
+        p {
+            margin-bottom: 23px;
+            font-size: 20px;
+        }
+    
+
+         button {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            color: #FFFFFF;
+            font-size: 18px;
+            width: 82px;
+            height: 43px;
+            background-color: #E8833A;
+            margin-right: 8px;
+            border: none;
+            border-radius: 3px;
+            cursor: pointer;
+        }
+`
+
+
+const AllSessions = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    justify-content: center;
+    
+   
+`;
 
 const Footer = styled.div`
     height: 117px;
--- src/theme/globalStyles.js
+++ src/theme/globalStyles.js
@@ -3,11 +3,11 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
 	@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400&display=swap');
     body{
-        background-color: #2C2F32;
+        background-color: #ffffff;
 		font-family: 'Roboto', sans-serif;
 		font-weight: 400;
-		/* color: #293845; */
-		color: aliceblue;
+		color: #293845;
+		
     }
 `;
 
